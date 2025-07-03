3f50211eae9053f4fe11e9dfb395165a5db6a736
marcbeep <77645923+marcbeep@users.noreply.github.com>
2025-07-04 00:16:01 +0100

    Fixed Circular Dep

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    esmExternals: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
