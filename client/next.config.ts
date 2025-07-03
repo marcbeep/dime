cba2261f02e12b67c46f3e9664836051f6b17f85
marcbeep <77645923+marcbeep@users.noreply.github.com>
2025-07-04 00:14:13 +0100

    Trying to fix Vercel Deploy Errs

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
